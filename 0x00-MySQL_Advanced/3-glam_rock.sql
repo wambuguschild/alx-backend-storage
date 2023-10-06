99d11eed783bfdb10731227b05b62c78ea61aa02
Jedidah <jedidahwambuguw@gmail.com>
2023-10-07 01:22:13 +0300

    Added tasks 2 to 5

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan from metal_bands WHERE style LIKE 'Glam rock%' ORDER BY lifespan DESC;
