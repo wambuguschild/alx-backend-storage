58044e54d0749626b70cb996add10eeda054d2ee
Jedidah <jedidahwambuguw@gmail.com>
2023-10-11 15:05:18 +0300

    Correction task 12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
-
-""" getting logs"""
+"""
+Provide some stats about Nginx logs stored in MongoDB
+Database: logs, Collection: nginx, Display same as example
+first line: x logs, x number of documents in this collection
+second line: Methods
+5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+one line with method=GET, path=/status
+"""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """ log stats"""
-    client = MongoClient('mongodb://localhost:27017')
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    db = client.logs
-    collection = db.nginx
 
-    total_logs = collection.count_documents({})
+def log_stats(mongo_collection, option=None):
+    """
+    Prototype: def log_stats(mongo_collection, option=None):
+    Provide some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
 
-    print(f"{total_logs} logs")
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
-
-    specific_method = "GET"
-    specific_path = "/status"
-    specific_count = collection.count_documents(
-        {"method": specific_method, "path": specific_path})
-    print(f"{specific_count} status check")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
